6a8f1dda9de8ba7fe68bd06323e38d2da9606636
saksham789 <saksham.vikram12@gmail.com>
2017-05-23 23:19:50 +0530

    Revert "Course Project 1"

--- plot1.R
+++
@@ -1,9 +0,0 @@
-data<-read.csv.sql(file ="household_power_consumption.txt",sql= "select * from file where Date in ('1/2/2007','2/2/2007') ",sep =";")
-librar(lubridate)
-h<-paste(data$Date,data$Time)
-g<-dmy_hms(h)
-data<-mutate(data,datetime=g)
-
-hist(data$Global_active_power,col ="red",xlab="Globalactivepower(killowatt)",main = "")
- dev.copy(png,file= "plot1.png")
-dev.off()
